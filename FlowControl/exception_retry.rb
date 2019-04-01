241df81eb0e8611371c75a08a75a3c3b2a23d883
Augusto Belotto Perini <aperini@gmail.com>
2019-04-01 15:34:15 -0400

    rescue, retry

---
+++ FlowControl/exception_retry.rb
@@ -0,0 +1,26 @@
+def method
+  execute
+  puts "success"
+  true
+rescue StandardError => e
+  attempts ||= 0
+  attempts += 1
+  puts "error"
+
+  if attempts <=1
+    # restarts execution of block where error occured
+    retry
+  else
+    puts "end retries"
+    false
+  end
+end
+
+def execute
+  if [true, false].sample
+    #error
+    puts "" + 1
+  end
+end
+
+puts method
