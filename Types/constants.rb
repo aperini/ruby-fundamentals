659aa035f76e0b5f5d3fd744403dc98c98e4df5d
Augusto Belotto Perini <augusto.belottoperini@lacapitale.com>
2019-04-09 15:55:31 -0400

    Types: constants

---
+++ Types/constants.rb
@@ -0,0 +1,27 @@
+# redefining a constant produces a warning
+MAX_SPEED = 1000
+
+# warning: already initialized constant MAX_SPEED
+MAX_SPEED = 10
+
+# Freezing it
+TYPES = ["abc"]
+TYPES.freeze
+
+# RTE  can't modify frozen Array (FrozenError)
+# TYPES << "def"
+
+puts "Is TYPES frozen? #{TYPES.frozen?}"
+
+# but... modifying an object inside a frozen object is ok
+TYPES[0].upcase!
+puts TYPES[0] # ABC
+
+# A constant defined outside a class or module is visible throughout the program
+# But to reference a constant from a class, we use the :: operator
+
+class A
+  MAX_SPEED = 1000
+end
+
+puts A::MAX_SPEED
